e57fae3a7ec16888b0a9cbfdcb32e5548c0e74b8
Kaushal Patel <krpatel21@outlook.com>
2016-08-10 10:26:46 -0500

    added new classes

---
+++ src/Item.java
@@ -0,0 +1,14 @@
+/**
+ * Created by p on 8/10/2016.
+ */
+public class Item
+{
+    int value;
+    int weight;
+
+    public Item(int value, int weight)
+    {
+        this.value = value;
+        this.weight = weight;
+    }
+}
---
+++ src/Node.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Node
+{
+    int level;
+    int profit;
+    int weight;
+    double bound;
+}
